ed990851631326ef9787c621882fef75ba9fdde8
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2025-12-27 13:29:09 +0530

    print a weak name by the help of the no. using switch condition

---
+++ Practice Problem/problem10.js
@@ -0,0 +1,26 @@
+let a=Math.floor(Math.random()*10)%7;
+switch(a){
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");               
+        break;
+    case 4:
+        console.log("Thrushday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid");
+}
