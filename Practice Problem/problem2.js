b2273c3485e5875ff2ab7fc468d2367485edf4ae
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2025-12-27 12:35:07 +0530

    take a input from the user print if the day of month id between 20 march to 20 june

--- Practice Problem/problem2.js
+++ Practice Problem/problem2.js
@@ -1,7 +1,4 @@
 let a =Number(process.argv[2]);
 let b = Number(process.argv[3]);
-if ((a >= 20 && a <= 30) && (b >= 3 && b <= 6)) {
-    console.log("True");
-} else {
-    console.log("False");
-}
+
+
