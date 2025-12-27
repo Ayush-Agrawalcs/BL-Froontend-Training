9f8be6a08df95929eded68ad9285d9732025cea1
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2025-12-27 13:09:28 +0530

    use if and else if condition to solve this

---
+++ Practice Problem/problem5.js
@@ -0,0 +1,32 @@
+let a=Math.floor(Math.random()*10);
+if(a==0){
+    console.log("Zero");
+}
+else if(a==1){
+    console.log("one")
+}
+else if(a==2){
+    console.log("Two")
+}
+else if(a==3){
+    console.log("Three")
+}
+else if(a==4){
+    console.log("Four")
+}
+else if(a==5){
+    console.log("Five") 
+}
+else if(a==6){
+    console.log("Six")
+}
+else if(a==7){
+    console.log("Seven")
+}
+else if(a==8){
+    console.log("Eight")
+}
+else{
+    console.log("Nine")
+}           
+
