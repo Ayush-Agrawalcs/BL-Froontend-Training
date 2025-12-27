6d744848779e3a72174989fc2afa4a7f4a923380
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2025-12-27 13:28:38 +0530

    print a weak name by the help of the no. using if else condition

--- Practice Problem/problem6.js
+++ Practice Problem/problem6.js
@@ -1,26 +1,15 @@
 let a=Math.floor(Math.random()*10)%7;
-switch(a){
-    case 0:
-        console.log("Sunday");
-        break;
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");               
-        break;
-    case 4:
-        console.log("Thrushday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    default:
-        console.log("Invalid");
-}
+if(a==0)
+    console.log("Sunday");
+else if(a==1)
+    console.log("Monday");
+else if(a==2)        
+    console.log("Tuesday");
+else if(a==3)
+    console.log("Wednesday");               
+else if(a==4)
+    console.log("Thrushday");
+else if(a==5)
+    console.log("Friday");
+else
+    console.log("Saturday");
