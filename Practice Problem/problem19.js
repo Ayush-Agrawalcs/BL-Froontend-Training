dc3652d8bd6425250257c53582a7a107e728ecc1
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2025-12-27 16:22:52 +0530

    just like a Binnary search using while loop

---
+++ Practice Problem/problem19.js
@@ -0,0 +1,17 @@
+let n=Number(process.argv[2]);
+let low=1;
+let high=100;
+while(low<=high){
+    console.log(low,high);
+    let mid=Math.floor((low+high)/2);
+    if(mid<n){
+        low=mid+1;
+    }
+    if(mid>n){
+        high=mid-1;
+    }
+    if(mid==n){
+        console.log("Found");
+        break;
+    }
+}
